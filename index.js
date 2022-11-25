5be26c95a6d38a93be5edc91b1954ac31c6e1f2d
RoyZiO <nziokinroy@gmail.com>
2022-11-25 09:07:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
+  let result
+  if (rideDistance <= 400) {
+    result = 'This one is on me!';
+    return result;
+
+  } else if (rideDistance > 2500) {
+    result = 'No can do.';
+    return result;
+
+  } else if (rideDistance >= 2000) {
+    result = 'I will gladly take your thirty bucks.';
+    return result;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  
+  return cityName === 'NYC'? "Ok, sounds good." : "No go."
 }
+ternaryCheckCity(true);
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(customerTip){
   // Write your code here!
+  switch (customerTip) {
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default: 'If anything else.'
+      return "Bye.";
+  }
 }
